4478d952d193dbe23f7c2b9be44d0bfe10e3030f
Christopher Peplin <chris.peplin@rhubarbtech.com>
2011-12-09 15:16:34 -0500

    Add support for chipKIT boards.

--- README.md
+++ README.md
@@ -8,6 +8,18 @@ The various released versions (0.3, 0.4, etc) are tagged in this repository.
 
 The original blog post: http://mjo.tc/atelier/2009/02/arduino-cli.html
 
+## chipKIT Support
+
+This version adds support for the Digilent chipKIT Uno32 and Max32, both
+Arduino-compatible microcontrollers. To use this Makefile with one of those,
+include `chipKIT.mk` instead of `Arduino.mk` at the bottom of your Makefile.
+
+    include /path/to/chipKIT.mk
+
+You can adjust the same variables as described by Martin for `Arduino.mk`, but
+point to an MPIDE installation (which includes the chipKIT toolchain) instead of
+the Arduino IDE.
+
 ## Dependencies
 
 The board feature detection script (`ard-parse-boards`) is a Perl script that
---
+++ chipKIT.mk
@@ -0,0 +1,33 @@
+#
+# chipKIT extensions for Arduino Makefile
+# System part (i.e. project independent)
+#
+# Copyright (C) 2011 Christopher Peplin <chris.peplin@rhubarbtech.com>,
+# based on work that is Copyright Martin Oldfield
+#
+# This file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 2.1 of the
+# License, or (at your option) any later version.
+#
+
+AVR_TOOLS_PATH = $(ARDUINO_DIR)/hardware/pic32/compiler/pic32-tools/bin
+ARDUINO_CORE_PATH = $(ARDUINO_DIR)/hardware/pic32/cores/pic32
+ARDUINO_LIB_PATH  = $(ARDUINO_SKETCHBOOK)/libraries
+BOARDS_TXT  = $(ARDUINO_DIR)/hardware/pic32/boards.txt
+
+CC_NAME = pic32-gcc
+CXX_NAME = pic32-g++
+AR_NAME = pic32-ar
+OBJDUMP_NAME = pic32-objdump
+OBJCOPY_NAME = pic32-objcopy
+
+AVRDUDE = $(ARDUINO_DIR)/hardware/tools/avrdude
+AVRDUDE_CONF = $(ARDUINO_DIR)/hardware/tools/avrdude.conf
+
+MCU_FLAG_NAME=mprocessor
+EXTRA_CPPFLAGS = -O2  -mno-smart-io -DARDUINO=22 -D_BOARD_MEGA_=
+
+CHIPKIT_MK_PATH := $(dir $(lastword $(MAKEFILE_LIST)))
+
+include $(CHIPKIT_MK_PATH)/Arduino.mk
