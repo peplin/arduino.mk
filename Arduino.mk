49f0606dcf97a33db54f8b87dfa910c55e258740
Christopher Peplin <cpeplin@ford.com>
2012-01-30 11:16:18 -0500

    Load BOARD definition from boards.txt instead of hard-coding for chipKIT.
    Fixed #5.

--- Arduino.mk
+++ Arduino.mk
@@ -208,12 +208,12 @@ AVRDUDE_ARD_PROGRAMMER = $(call PARSE_BOARD,$(BOARD_TAG),upload.protocol)
 endif
 
 ifndef AVRDUDE_ARD_BAUDRATE
-AVRDUDE_ARD_BAUDRATE   = $(call PARSE_BOARD,$(BOARD_TAG),upload.speed)
+AVRDUDE_ARD_BAUDRATE = $(call PARSE_BOARD,$(BOARD_TAG),upload.speed)
 endif
 
 # fuses if you're using e.g. ISP
 ifndef ISP_LOCK_FUSE_PRE
-ISP_LOCK_FUSE_PRE  = $(call PARSE_BOARD,$(BOARD_TAG),bootloader.unlock_bits)
+ISP_LOCK_FUSE_PRE = $(call PARSE_BOARD,$(BOARD_TAG),bootloader.unlock_bits)
 endif
 
 ifndef ISP_LOCK_FUSE_POST
@@ -221,19 +221,23 @@ ISP_LOCK_FUSE_POST = $(call PARSE_BOARD,$(BOARD_TAG),bootloader.lock_bits)
 endif
 
 ifndef ISP_HIGH_FUSE
-ISP_HIGH_FUSE      = $(call PARSE_BOARD,$(BOARD_TAG),bootloader.high_fuses)
+ISP_HIGH_FUSE = $(call PARSE_BOARD,$(BOARD_TAG),bootloader.high_fuses)
 endif
 
 ifndef ISP_LOW_FUSE
-ISP_LOW_FUSE       = $(call PARSE_BOARD,$(BOARD_TAG),bootloader.low_fuses)
+ISP_LOW_FUSE = $(call PARSE_BOARD,$(BOARD_TAG),bootloader.low_fuses)
 endif
 
 ifndef ISP_EXT_FUSE
-ISP_EXT_FUSE       = $(call PARSE_BOARD,$(BOARD_TAG),bootloader.extended_fuses)
+ISP_EXT_FUSE = $(call PARSE_BOARD,$(BOARD_TAG),bootloader.extended_fuses)
 endif
 
 ifndef VARIANT
-VARIANT            = $(call PARSE_BOARD,$(BOARD_TAG),build.variant)
+VARIANT = $(call PARSE_BOARD,$(BOARD_TAG),build.variant)
+endif
+
+ifndef BOARD
+BOARD = $(call PARSE_BOARD,$(BOARD_TAG),board)
 endif
 
 # Everything gets built in here
