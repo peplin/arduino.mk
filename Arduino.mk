2bf19ad0ef17e80d2a141f3f24e9cd90eb9b6688
Christopher Peplin <cpeplin@ford.com>
2012-02-22 10:50:26 -0500

    Automated ARDUINO_LIBS collection from Ed Comer.
    Ed writes:
    
    I made another change. I got tired of setting up a make process and discovering
    that  ARDUINO_LIBS was wrong, so I automated that also. Now the makefile
    searches the source file for includes, sees if they are resident in the
    libraries folder and if they are, creates a ARDUINO_LIBS variable populated with
    the libraries imported by the source file. I've attached my current Arduino.mk

--- Arduino.mk
+++ Arduino.mk
@@ -1,5 +1,5 @@
 ########################################################################
-#
+# Version 02.19.2012
 # Arduino command line tools Makefile
 # System part (i.e. project independent)
 #
@@ -14,22 +14,33 @@
 # Adapted from Arduino 0011 Makefile by M J Oldfield
 # Original Arduino adaptation by mellis, eighthave, oli.keller
 # Modified by Christopher Peplin for chipKIT.
-# Further modified by Edward Comer
+# Further modified by Edward Comer for more automation
 ########################################################################
 # STANDARD ARDUINO WORKFLOW
 #
-# Given a normal sketch directory, all you need to do is to create
-# a small Makefile which defines a few things, and then includes this one.
+# Given a normal sketch directory, where the program filename is the same
+# as the directory name, plus a suffix of .ino or .pde, all you need to 
+# do is to create a small Makefile like the example below and place it onto
+# the sketch directory of the target program. However, if your sketch
+# program filename differs from the basename of the directory that it 
+# resides in, you must define TARGET, for example:
+# TARGET = BaseNameOfSketchFile
 #
 # For example:
-#       ARDUINO_DIR  = /Applications/arduino-0013
-#       TARGET       = CLItest
-#       ARDUINO_LIBS = LiquidCrystal
-#
-#       BOARD_TAG    = uno
-#       ARDUINO_PORT = /dev/cu.usb*
-#
-#       include /usr/local/share/Arduino.mk
+#----------Sample Makefile placed within Arduino sketchbook-----------
+## BOARD_TAG: do make show_boards to list all valid boards
+#BOARD_TAG    = nano328
+#ARDUINO_DIR  = /home/user/arduino/arduino-1.0
+#ARDUINO_SKETCHBOOK = /home/user/arduino/arduino-1.0/sketchbook
+#ARDUINO_MAKEFILE_HOME = /home/user/arduino
+#ARDUINO_LIBS = SoftwareSerial
+#ARDUINO_PORT = /dev/ttyUSB*
+
+## .SECONDARY: will cause the intermediary files to be kept
+##.SECONDARY:
+
+#include $(ARDUINO_MAKEFILE_HOME)/Arduino.mk
+#----------------------------------------------------------------------
 #
 # Hopefully these will be self-explanatory but in case they're not:
 #
@@ -40,11 +51,11 @@
 #    ARDUINO_LIBS - A list of any libraries used by the sketch (we assume
 #                   these are in $(ARDUINO_DIR)/hardware/libraries
 #    ARDUINO_PORT - The port where the Arduino can be found. Only needed
-#                   when uploading
+#                   if overriding avrdude.conf
 #    BOARD_TAG    - The tag for the board e.g. uno or mega
-#                   'make show_boards' shows a list
+#                   'make showboards' shows a list
 #
-# You might also want to specify these, but normally they'll be read from the
+# You can also specify these, but normally they'll be read from the
 # boards.txt file i.e. implied by BOARD_TAG
 #
 #    MCU,F_CPU    - The target processor description
@@ -52,6 +63,8 @@
 # Once this file has been created the typical workflow is just
 #
 #   $ make upload
+#	or
+#	$ make ispload
 #
 # All of the object files are created in the build-cli subdirectory
 # All sources should be in the current directory and can include:
@@ -67,88 +80,97 @@
 #   make depends     - update dependencies
 #   make reset       - reset the Arduino by tickling DTR on the serial port
 #   make raw_upload  - upload without first resetting
-#   make show_boards - list all the boards defined in boards.txt
+#   make showboards - list all the boards defined in boards.txt
 #   make ispload     - upload via external programmer
 #
 ########################################################################
 # ARDUINO WITH OTHER TOOLS
 #
-# If the tools aren't in the Arduino distribution, then you need to
+# If the tools aren't in the Arduino distribution, AND are not in your
+# computor's normal execution path, then you need to
 # specify their location:
-#
 #    AVR_TOOLS_PATH = /usr/bin
 #    AVRDUDE_CONF   = /etc/avrdude/avrdude.conf
 ########################################################################
 # ARDUINO WITH ISP
 #
-# You need to specify some details of your ISP programmer and might
-# also need to specify the fuse values:
+# Values similar to the following SHOULD be automatically retrieved 
+# from avrdude.conf. However, you can manually set them to override 
+# avrdude.conf.
 #
+# For example only (values for illustration only):
+#     ARDUINO_PORT = /dev/ttyUSB*
 #     ISP_PROG	   = -c stk500v2
 #     ISP_PORT     = /dev/ttyACM0
-#
-# You might also need to set the fuse bits, but typically they'll be
-# read from boards.txt, based on the BOARD_TAG variable:
-#
 #     ISP_LOCK_FUSE_PRE  = 0x3f
 #     ISP_LOCK_FUSE_POST = 0xcf
 #     ISP_HIGH_FUSE      = 0xdf
 #     ISP_LOW_FUSE       = 0xff
 #     ISP_EXT_FUSE       = 0x01
 #
-# I think the fuses here are fine for uploading to the ATmega168
-# without bootloader.
-#
-# To actually do this upload use the ispload target:
+# To actually do an upload use the ispload target:
 #
 #    make ispload
 ########################################################################
 # Some paths
 #
+#WARNING: This makefile is bash shell dependant!
+SHELL=/bin/bash
 
-# Since the Makefile MUST specify ARDUINO_DIR, whu is this condition here?
-ifneq (ARDUINO_DIR,)
-
-	ifndef AVR_TOOLS_PATH
-		AVR_TOOLS_PATH = $(shell dirname "`find $(ARDUINO_DIR) -type f -name 'avr-gcc'`")
-	endif
-
-	ifndef AVRDUDE_CONF
-		AVRDUDE_CONF = $(shell dirname "`find $(ARDUINO_DIR) -type f -name 'avrdude.conf'`")
-	endif
-
-	ifndef ARDUINO_LIB_PATH
-		ARDUINO_LIB_PATH  = $(ARDUINO_DIR)/libraries
-	endif
-
-	ifndef USER_LIB_PATH
-		USER_LIB_PATH = $(ARDUINO_SKETCHBOOK)/libraries
-	endif
-
-	ARDUINO_CORE_PATH = $(shell find $(ARDUINO_DIR) -type d -name 'arduino' | sort -u|grep cores)
-
-	# Sketchbook location needed to descriminate between ~/arduino/arduino-1.0/hardware/arduino/variants
-	# and varients within sketchbook per MIT's High-Low tech http://hlt.media.mit.edu/?p=1695
-	# https://github.com/damellis/attiny/tree/Arduino1
-	ifndef ARDUINO_SKETCHBOOK
-		ARDUINO_SKETCHBOOK = $(ARDUINO_DIR)/sketchbook
-	endif
-
-	ifndef VARIANTS_PATH
-		ifneq (,$(findstring tiny,$(BOARD_TAG)))
-			VARIANTS_PATH = $(shell find $(ARDUINO_SKETCHBOOK) -type d -name 'variants')
-		else
-			VARIANTS_PATH = $(ARDUINO_DIR)/hardware/arduino/variants
-		endif
-	endif
+ifeq ($(wildcard *.ino),)
+	SUFFIX:= pde
+else
+	SUFFIX:= ino
 endif
 
-# Select suffix type
+ifndef TARGET
+	TARGET := $(notdir $(CURDIR))
+endif
+	
+ifndef AVR_TOOLS_PATH
+	AVR_TOOLS_PATH := $(shell dirname "`find $(ARDUINO_DIR) -type f -name 'avr-gcc'`")
+endif
+
+ifndef AVRDUDE_CONF
+	AVRDUDE_CONF := $(shell find $(ARDUINO_DIR) -type f -name 'avrdude.conf')
+endif
+
+ifndef ARDUINO_LIBS
+	ARDUINO_LIBS := $(shell for f in $$(grep "\#include" $(TARGET).$(SUFFIX)); \
+	do if [ "$$f" = "\#include" ]; \
+	then continue; \
+	fi; \
+	ff=$$(echo "$$f"|sed "s:.\#include.*<::"|tr -d "<\">"); \
+	echo -n "$$(basename "$$(find "/home/ecomer/arduino/arduino-1.0/libraries" -name "$${ff}")" .h) "; \
+	done )
+endif
+
+ifndef ARDUINO_LIB_PATH
+	ARDUINO_LIB_PATH  := $(ARDUINO_DIR)/libraries
+endif
+
+ifndef USER_LIB_PATH
+	USER_LIB_PATH := $(ARDUINO_SKETCHBOOK)/libraries
+endif
+
+ARDUINO_CORE_PATH := $(shell find $(ARDUINO_DIR) -type d -name 'arduino' | sort -u|grep cores)
+
+# Sketchbook location needed to descriminate between ~/arduino/arduino-1.0/hardware/arduino/variants
+# and varients within sketchbook per MIT's High-Low tech http://hlt.media.mit.edu/?p=1695
+# https://github.com/damellis/attiny/tree/Arduino1
+ifndef ARDUINO_SKETCHBOOK
+	ARDUINO_SKETCHBOOK := $(ARDUINO_DIR)/sketchbook
+endif
+
+ifndef VARIANTS_PATH
+	ifneq (,$(findstring tiny,$(BOARD_TAG)))
+		VARIANTS_PATH := $(shell find $(ARDUINO_SKETCHBOOK) -type d -name 'variants')
+	else
+		VARIANTS_PATH := $(ARDUINO_DIR)/hardware/arduino/variants
+	endif
+endif
 ifndef ARDUINO_VERSION
 	ARDUINO_VERSION = 100
-	SUFFIX := ino
-else
-	SUFFIX := pde
 endif
 
 ARDUINO_MK_PATH := $(dir $(lastword $(MAKEFILE_LIST)))
@@ -165,9 +187,9 @@ endif
 # CHANGED
 ifndef BOARDS_TXT
 	ifneq (,$(findstring tiny,$(BOARD_TAG)))
-		BOARDS_TXT = $(shell find $(ARDUINO_DIR) -type f -name 'boards.txt' |sort -u |grep tiny)
+		BOARDS_TXT := $(shell find $(ARDUINO_DIR) -type f -name 'boards.txt' |sort -u |grep tiny)
 	else
-		BOARDS_TXT = $(shell find $(ARDUINO_DIR) -type f -name 'boards.txt' |sort -u |grep -v tiny)
+		BOARDS_TXT := $(shell find $(ARDUINO_DIR) -type f -name 'boards.txt' |sort -u |grep -v tiny)
 	endif
 endif
 
@@ -183,43 +205,43 @@ endif
 
 # processor stuff
 ifndef MCU
-	MCU   = $(call PARSE_BOARD,$(BOARD_TAG),build.mcu)
+	MCU   := $(call PARSE_BOARD,$(BOARD_TAG),build.mcu)
 endif
 
 ifndef F_CPU
-	F_CPU = $(call PARSE_BOARD,$(BOARD_TAG),build.f_cpu)
+	F_CPU := $(call PARSE_BOARD,$(BOARD_TAG),build.f_cpu)
 endif
 
 # normal programming info
-ifndef AVRDUDE_ARD_PROGRAMMER
-	AVRDUDE_ARD_PROGRAMMER = $(call PARSE_BOARD,$(BOARD_TAG),upload.protocol)
+ifndef AVRDUDE_PROTOCOL
+	AVRDUDE_PROTOCOL := $(call PARSE_BOARD,$(BOARD_TAG),upload.protocol)
 endif
 
 # FIX ME - May not be present
-ifndef AVRDUDE_ARD_BAUDRATE
-	AVRDUDE_ARD_BAUDRATE = $(call PARSE_BOARD,$(BOARD_TAG),upload.speed)
+ifndef AVRDUDE_PORT_BAUDRATE
+	AVRDUDE_PORT_BAUDRATE := $(call PARSE_BOARD,$(BOARD_TAG),upload.speed)
 endif
 
 # FIX ME - May not be present
 ifndef ISP_LOCK_FUSE_PRE
-	ISP_LOCK_FUSE_PRE = $(call PARSE_BOARD,$(BOARD_TAG),bootloader.unlock_bits)
+	ISP_LOCK_FUSE_PRE := $(call PARSE_BOARD,$(BOARD_TAG),bootloader.unlock_bits)
 endif
 
 # FIX ME - May not be present
 ifndef ISP_LOCK_FUSE_POST
-	ISP_LOCK_FUSE_POST = $(call PARSE_BOARD,$(BOARD_TAG),bootloader.lock_bits)
+	ISP_LOCK_FUSE_POST := $(call PARSE_BOARD,$(BOARD_TAG),bootloader.lock_bits)
 endif
 
 ifndef ISP_HIGH_FUSE
-	ISP_HIGH_FUSE = $(call PARSE_BOARD,$(BOARD_TAG),bootloader.high_fuses)
+	ISP_HIGH_FUSE := $(call PARSE_BOARD,$(BOARD_TAG),bootloader.high_fuses)
 endif
 
 ifndef ISP_LOW_FUSE
-	ISP_LOW_FUSE = $(call PARSE_BOARD,$(BOARD_TAG),bootloader.low_fuses)
+	ISP_LOW_FUSE := $(call PARSE_BOARD,$(BOARD_TAG),bootloader.low_fuses)
 endif
 
 ifndef ISP_EXT_FUSE
-	ISP_EXT_FUSE = $(call PARSE_BOARD,$(BOARD_TAG),bootloader.extended_fuses)
+	ISP_EXT_FUSE := $(call PARSE_BOARD,$(BOARD_TAG),bootloader.extended_fuses)
 endif
 
 ifdef KEEPFUSES
@@ -231,24 +253,25 @@ else
 endif
 
 ifndef VARIANT
-	VARIANT = $(call PARSE_BOARD,$(BOARD_TAG),build.variant)
+	VARIANT := $(call PARSE_BOARD,$(BOARD_TAG),build.variant)
 endif
 
 ########################################################################
 #
-# Avrdude
+# Avrdude parameter preparation
 #
 
 ifndef AVRDUDE
 	ifeq ($(AVR_TOOLS_PATH),.)
-		AVRDUDE = $(shell find $(ARDUINO_DIR) -type f -name 'avrdude')
+		AVRDUDE := $(shell find $(ARDUINO_DIR) -type f -name 'avrdude')
 	else
-		AVRDUDE = $(shell find $(AVR_TOOLS_PATH) -type f -name 'avrdude')
+		AVRDUDE := $(shell find $(AVR_TOOLS_PATH) -type f -name 'avrdude')
 	endif
 endif
 
-AVRDUDE_COM_OPTS = -p $(MCU)
+AVRDUDE_COM_OPTS := -p $(MCU)
 
+AVRDUDE_ISP_OPTS := -P $(ISP_PORT) $(ISP_PROG)
 
 #######################################################################
 #
@@ -265,10 +288,10 @@ endif
 
 
 # Everything gets built in here
-OBJDIR  	  = build-cli
+OBJDIR  	  := build-cli
 
 ########################################################################
-# Local sources
+# Local sourcesifeq ($(strip $(NO_CORE)),)
 #
 LOCAL_C_SRCS    = $(wildcard *.c)
 LOCAL_CPP_SRCS  = $(wildcard *.cpp)
@@ -391,7 +414,7 @@ LDFLAGS       = -$(MCU_FLAG_NAME)=$(MCU) -lm -Wl,--gc-sections -Os $(EXTRA_LDFLA
 PDEHEADER     = \\\#include \"$(CORE_INCLUDE_NAME)\"
 
 # Expand and pick the first port
-ARD_PORT      = $(firstword $(wildcard $(ARDUINO_PORT)))
+AVRDUDE_PORT      := $(firstword $(wildcard $(ARDUINO_PORT)))
 
 # Implicit rules for building everything (needed to get everything in
 # the right directory)
@@ -496,29 +519,32 @@ $(TARGET_ELF): 	$(OBJS)
 $(DEP_FILE):	$(OBJDIR) $(DEPS)
 		@cat $(DEPS) > $(DEP_FILE)
 
+# reset the Arduino by tickling DTR on the serial port
+# then upload the hex file via avrdude
 upload:		reset raw_upload
 
 raw_upload:	$(TARGET_HEX)
-		$(AVRDUDE) $(AVRDUDE_COM_OPTS) $(AVRDUDE_ARD_OPTS) \
-			-U flash:w:$(TARGET_HEX):i
+		$(AVRDUDE) -C$(AVRDUDE_CONF) -v -v -v  -p $(MCU) \
+		-c $(AVRDUDE_PROTOCOL) -P $(AVRDUDE_PORT) -b $(AVRDUDE_PORT_BAUDRATE) \
+			-D -U flash:w:$(TARGET_HEX):i
 
 # stty on MacOS likes -F, but on Debian it likes -f redirecting
 # stdin/out appears to work but generates a spurious error on MacOS at
 # least. Perhaps it would be better to just do it in perl ?
 reset:
-		@if [ -z "$(ARD_PORT)" ]; then \
+		@if [ -z "$(AVRDUDE_PORT)" ]; then \
 			echo "No Arduino-compatible TTY device found -- exiting"; exit 2; \
 			fi
 		for STTYF in 'stty --file' 'stty -f' 'stty <' ; \
 		  do $$STTYF /dev/tty >/dev/null 2>/dev/null && break ; \
 		done ;\
-		$$STTYF $(ARD_PORT)  hupcl ;\
+		$$STTYF $(AVRDUDE_PORT)  hupcl ;\
 		(sleep 0.1 || sleep 1)     ;\
-		$$STTYF $(ARD_PORT) -hupcl
+		$$STTYF $(AVRDUDE_PORT) -hupcl
 
 ispload:	$(TARGET_HEX)
-		$(AVRDUDE) $(AVRDUDE_COM_OPTS) $(AVRDUDE_ISP_OPTS) -v -v -v \
-			-c $(AVRDUDE_ARD_PROGRAMMER) $(FUSE_OPTS) \
+		$(AVRDUDE) -p $(MCU) $(AVRDUDE_ISP_OPTS) -v -v -v \
+			-c $(AVRDUDE_PROTOCOL) $(FUSE_OPTS) \
 			-U flash:w:$(TARGET_HEX):i
 
 serial:
@@ -530,12 +556,14 @@ clean:
 depends:	$(DEPS)
 		@cat $(DEPS) > $(DEP_FILE)
 
-show_boards:
+showboards:
 	@cat $(BOARDS_TXT) | grep -E "^[[:alnum:]]" | cut -d . -f 1 | uniq
-	
+
+# Next target is for debugging. For example, Entering make print-AVRDUDE_CONF
+# yields: AVRDUDE_CONF = [/yourpath/hardware/tools/avrdude.conf]
 print-%:
 	@echo $* = [$($*)]
 
-.PHONY:	all clean depends upload raw_upload reset show_boards
+.PHONY:	all clean depends upload raw_upload reset showboards
 
 include $(DEP_FILE)
